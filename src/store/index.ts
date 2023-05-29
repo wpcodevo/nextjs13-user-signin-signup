71371abe2c4a9b412be7dcacd71776a17728945d
wpcodevo <ziddah.edem@gmail.com>
2023-05-29 13:11:23 +0000

    updated

--- src/store/index.ts
+++ src/store/index.ts
@@ -6,21 +6,17 @@ import { create } from "zustand";
 type Store = {
   authUser: FilteredUser | null;
   requestLoading: boolean;
-  tokenValid: boolean;
   setAuthUser: (user: FilteredUser | null) => void;
   setRequestLoading: (isLoading: boolean) => void;
-  setTokenValid: (valid: boolean) => void;
   reset: () => void;
 };
 
 const useStore = create<Store>((set) => ({
   authUser: null,
-  tokenValid: false,
   requestLoading: false,
   setAuthUser: (user) => set((state) => ({ ...state, authUser: user })),
   setRequestLoading: (isLoading) =>
     set((state) => ({ ...state, requestLoading: isLoading })),
-  setTokenValid: (valid) => set((state) => ({ ...state, tokenValid: valid })),
   reset: () => set({ authUser: null, requestLoading: false }),
 }));
 
